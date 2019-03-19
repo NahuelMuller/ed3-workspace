9394fdfc444cb0e5d5339ac88e5af2fd0d093f10
NahuelMuller <nahuelam.93@gmail.com>
2019-03-19 08:48:43 -0300

    ed3-looperZ: FIR working (limited input range)

--- ed3-looperZ/app/src/dsp_functions.c
+++ ed3-looperZ/app/src/dsp_functions.c
@@ -1,43 +1,36 @@
 /*====== LIBRARIES ======*/
 #include	"main.h"			// Header
 
-/*
-FIR filter designed with
-http://t-filter.appspot.com
-sampling frequency: 7994 Hz
-fixed point precision: 15 bits
-* 0 Hz - 400 Hz
-  gain = 1
-  desired ripple = 5 dB
-  actual ripple = n/a
-* 800 Hz - 3997 Hz
-  gain = 0
-  desired attenuation = -40 dB
-  actual attenuation = n/a
-*/
+#define FILTER_TAP_NUM 29
 
-#define FILTER_TAP_NUM 28
-
-int16_t filter_taps[FILTER_TAP_NUM] = {
-  -127,  -130,  -157,  -145,  -73,  75,  306,  615, 981,  1369,  1738,
-  2042,  2242,  2312,  2242,  2042,  1738,  1369,  981,  615,  306,
-  75,  -73,  -145,  -157,  -130,  -127, 0
+const float32_t filter_taps[FILTER_TAP_NUM] = {										// MATLAB: fir1(28, 1/4)
+		-0.001822523039f, -0.001587929377f, +0.000000000000f, +0.003697750828f,
+		+0.008075430263f, +0.008530221683f, +0.000000000000f, -0.017397698394f,
+		-0.034145860704f, -0.033359156473f, +0.000000000000f, +0.067630839471f,
+		+0.152206183469f, +0.222924695624f, +0.250496093294f, +0.222924695624f,
+		+0.152206183469f, +0.067630839471f, +0.000000000000f, -0.033359156473f,
+		-0.034145860704f, -0.017397698394f, +0.000000000000f, 0.008530221683f,
+		+0.008075430263f, +0.003697750827f, +0.000000000000f, -0.001587929377f,
+		-0.001822523039f
 };
 
-arm_fir_instance_q15 fir_instance;						// Instance structure for the Q15 FIR filter.
-int16_t fir_state[SAMPLES_BUFFER + FILTER_TAP_NUM];		// State buffer
+
+arm_fir_instance_f32 fir_instance;								// Instance structure for the f32 FIR filter.
+static float32_t fir_state[SAMPLES_BUFFER + FILTER_TAP_NUM];	// State buffer
+static float32_t buffer1[SAMPLES_BUFFER], buffer2[SAMPLES_BUFFER];
 
 /*====== FUNCTION DECLARATIONS ======*/
 
 void dsp_filter_init(void){
 
-	// The function returns ARM_MATH_SUCCESS if initialization is successful or ARM_MATH_ARGUMENT_ERROR if numTaps is not greater than or equal to 4 and even.
-	arm_fir_init_q15(&fir_instance, FILTER_TAP_NUM, &filter_taps[0], &fir_state[0], SAMPLES_BUFFER);
+	arm_fir_init_f32(&fir_instance, FILTER_TAP_NUM, (float32_t *) &filter_taps[0], &fir_state[0], SAMPLES_BUFFER);
 
 }
 
 void dsp_filter(uint16_t *input, uint16_t *output){
 
-	arm_fir_q15(&fir_instance, input, output, SAMPLES_BUFFER);
+	arm_q15_to_float((q15_t *) &input[0], buffer1, SAMPLES_BUFFER);
+	arm_fir_f32(&fir_instance, buffer1, buffer2, SAMPLES_BUFFER);
+	arm_float_to_q15(buffer2, (q15_t *) &output[0], SAMPLES_BUFFER);
 
 }
