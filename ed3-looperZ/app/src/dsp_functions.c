9235c72887686aef55ee2742702fb7cdd96275c1
NahuelMuller <nahuelam.93@gmail.com>
2019-03-26 13:39:00 -0300

    ed3-lopperZ: Changed FIR filter type

--- ed3-looperZ/app/src/dsp_functions.c
+++ ed3-looperZ/app/src/dsp_functions.c
@@ -3,7 +3,9 @@
 
 #define FILTER_TAP_NUM 29
 
-const float32_t filter_taps[FILTER_TAP_NUM] = {										// MATLAB: fir1(28, 1/4)
+/*
+//LOW PASS fc = 1KHz
+const float32_t filter_taps[FILTER_TAP_NUM] = {		// MATLAB: fir1(28, 1/4) = fir1(orden_filtro, f_corte/(0.5 * f_sampling))
 		-0.001822523039f, -0.001587929377f, +0.000000000000f, +0.003697750828f,
 		+0.008075430263f, +0.008530221683f, +0.000000000000f, -0.017397698394f,
 		-0.034145860704f, -0.033359156473f, +0.000000000000f, +0.067630839471f,
@@ -13,7 +15,19 @@ const float32_t filter_taps[FILTER_TAP_NUM] = {										// MATLAB: fir1(28, 1/4
 		+0.008075430263f, +0.003697750827f, +0.000000000000f, -0.001587929377f,
 		-0.001822523039f
 };
+*/
 
+//HIGH PASS fc = 100Hz
+const float32_t filter_taps[FILTER_TAP_NUM] = {		// MATLAB: fir1(28, 100/(0.5*7993.73), 'high') = fir1(orden_filtro, f_corte/(0.5 * f_sampling), tipo)
+		-0.001623970158f, -0.001914954240f, -0.002700136391f, -0.003977249409f,
+		-0.005711517871f, -0.007836731895f, -0.010258528748f, -0.012859700415f,
+		-0.015507194114f, -0.018060344058f, -0.020379776663f, -0.022336375435f,
+		-0.023819680468f, -0.024745132218f, +0.976540633486f, -0.024745132218f,
+		-0.023819680468f, -0.022336375435f, -0.020379776663f, -0.018060344058f,
+		-0.015507194114f, -0.012859700415f, -0.010258528748f, -0.007836731895f,
+		-0.005711517871f, -0.003977249409f, -0.002700136391f, -0.001914954240f,
+		-0.001623970158f
+};
 
 arm_fir_instance_f32 fir_instance;								// Instance structure for the f32 FIR filter.
 static float32_t fir_state[SAMPLES_BUFFER + FILTER_TAP_NUM];	// State buffer
